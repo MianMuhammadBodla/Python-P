abea90f3bb2d349ecb77bb42a0c5f5c54b1a0bda
MianMuhammadBodla <mianmuhammadbodla@gmail.com>
2024-03-16 02:39:50 +0500

    first commit

---
+++ uit-api-class/uitclass/Practice/g.py
@@ -0,0 +1,52 @@
+# print("Hello World")
+
+# name : str = ("Muhammad")
+
+# print("My name is " + name)
+
+# print ('"By love serve one another"')
+
+# print ("'By love serve one another'")
+
+# print ('''MY NAME IS MUHAMMAD
+# IM STUDENT OF FCCU''')
+
+# a : int = 7
+# b : int = 2
+
+# print(a + b) # addtition
+# print(a - b) # subtraction
+# print(a * b) # multiplication
+# print(a / b) # division
+
+# fruits = ['apple', 'banana', 'cherry','mango', 'nanana', 'melon']
+
+# print (fruits.sort())
+# print(fruits)
+
+# fruits.append('grape')
+# print(fruits)
+
+
+# fruits_list = list(fruits)
+# fruits_list.append('Watermelon')
+# fruits = tuple(fruits_list)
+# print(fruits)
+
+
+# x: int =0
+# while x <= 10:
+#     print('Muhammad')
+#     x = x + 1
+
+# x: int = 0
+# for x in range(10):
+#     print('Muhammad')
+
+
+x: int = 0
+for x in [1,2,3,4,5,6,7,8,9]:
+    print('Muhammad')
+    
+
+
---
+++ uit-api-class/uitclass/Practice/hello.py
@@ -0,0 +1,75 @@
+# name: str = 'Muhammad'
+
+# print ("My name is ", name)
+
+# print ("This is our class")
+
+
+# print ("This is our class")
+
+# print ('"By Love Serve One Another"')
+
+# print ( ''' Be Patient
+#         You Are Right'''
+# )
+
+
+# print ('Piaic', 'Generative AI', 'Cloud Applied', sep='---')
+
+# print( 'Piaic ', end='   ')
+
+
+# print('GenAI', end='   ')
+
+# print('Cloud Applied')
+
+# a : int = 7
+# b : int = 2
+
+# print(a + b) # addtition
+# print(a - b) # subtraction
+# print(a * b)
+# print(a / b) # division
+
+
+
+
+
+# fruits: list[str] = ['apple ', 'orange',
+#                       'cherry' , 'mango', 'banana', 'melon',]  
+
+# # print (fruits)
+
+# # print (fruits[2])
+
+# # print (fruits [-1])
+
+# # print (fruits[3:5])
+
+# # print(fruits[-3:-5])
+
+# # print(fruits [0::2])
+# print(fruits)
+# print (fruits.sort())
+# print(fruits)
+
+# fruits_list = list(fruits)
+# fruits_list.append('Watermelon')
+# fruits = tuple(fruits_list)
+# print(fruits)
+
+
+# x: int =0
+# while x <= 10:
+#     print('Muhammad')
+#     x = x + 1
+
+
+# x: int = 0
+# for x in range(10):
+#     print('Muhammad')
+
+# x: int = 0
+# for x in [1,2,3,4,5,6,7,8,9]:
+#     print('Muhammad')
+    
